aa61eebee556d415be0da5304c328629d67fbb6e
Dan Busby <Dan Busby>
2016-10-17 12:52:10 -0400

    Final version

--- web.py
+++ web.py
@@ -4,37 +4,26 @@ import giphypop
 app = Flask(__name__)
 g = giphypop.Giphy()
 
-
-# def get_gif(term):
-#     responses = g.search(term)
-#     # for response in responses:
-#     #     return(response.media_url)
-#     #     return(response.url)
-  
+#Displays the greeting when a user goes to the index page
 @app.route('/')
 def index():
-    name = request.values.get('name', 'You Sexy Beast')
+    name = request.values.get('name', 'You Sexy Beast') #Builds confidence of user
     greeting = "Hello {}".format(name)
     return render_template('index.html', greeting=greeting)
 
+#Routes user from index page to about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#Routes user from index to results page once a user has submitted a search term
 @app.route('/results')
 def results():
     term = request.values.get('term') 
     responses = g.search(term)
-    reply = 'GIFs tagged with "{}"'.format(term)
+    reply = 'GIFs tagged with "{}"'.format(term) #Customizes response based on search term
     return render_template('results.html', responses=responses, reply=reply)
 
-# @app.route('/results')
-# def results():
-#     term = request.values.get('term')
-#     reply = "GIFs tagged with {}".format(term)
-#     responses = get_gif(term)
-#     return render_template('results.html', responses=responses, reply=reply)
-
-# app.run(debug=True)
+#Code to push to Heroku
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
